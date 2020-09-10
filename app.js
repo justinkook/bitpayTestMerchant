29cfc05d8b5d4ef3bdd11a3d092e1d4be8f1b800
Justin Kook <jkook@bitpay.com>
2020-09-10 15:06:45 -0400

    IIFE eventlistener on form

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-async function generateInvoice(event) {
-    event.preventDefault();
-    const invoiceId = await axios.post('https://staging.bitpay.com/checkout', {
-        action: 'checkout',
-        posData: '',
-        data: 'YzNHKdC1iQeZoR3gfFxF1N0JyhglsfO6hyFIawpMSUVdNRBBiu2A8/EdM6E0GqvQJ7VeSdeL5+KZTkEo+/eaiOR2mMH79hzVdCPfwHvcyECNibmB0WQTrqliB4abGlRYtplunFqpg2XnFzzcDSvHPQ=='
-    });
-    bitpay.showInvoice(invoiceId);
-}
+(function() {
+    const bitpayForm = document.getElementById('bitpayButton');
+    bitpayForm.addEventListener('click', generateInvoice);
+    
+    async function generateInvoice(event) {
+        event.preventDefault();
+        const invoiceId = await axios.post('https://staging.bitpay.com/checkout', {
+            action: 'checkout',
+            posData: '',
+            data: 'YzNHKdC1iQeZoR3gfFxF1N0JyhglsfO6hyFIawpMSUVdNRBBiu2A8/EdM6E0GqvQJ7VeSdeL5+KZTkEo+/eaiOR2mMH79hzVdCPfwHvcyECNibmB0WQTrqliB4abGlRYtplunFqpg2XnFzzcDSvHPQ=='
+        });
+        bitpay.showInvoice(invoiceId);
+    }
+})();

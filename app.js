49f212e311f97b4b9f0da20e1b5d79b748ec3f04
Justin Kook <jkook@bitpay.com>
2020-09-10 15:14:22 -0400

    try catch around POST request

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 (function() {
     const bitpayForm = document.getElementById('bitpayButton');
     bitpayForm.addEventListener('click', generateInvoice);
-    
+
     async function generateInvoice(event) {
         event.preventDefault();
-        const invoiceId = await axios.post('https://staging.bitpay.com/checkout', {
-            action: 'checkout',
-            posData: '',
-            data: 'YzNHKdC1iQeZoR3gfFxF1N0JyhglsfO6hyFIawpMSUVdNRBBiu2A8/EdM6E0GqvQJ7VeSdeL5+KZTkEo+/eaiOR2mMH79hzVdCPfwHvcyECNibmB0WQTrqliB4abGlRYtplunFqpg2XnFzzcDSvHPQ=='
-        });
-        bitpay.showInvoice(invoiceId);
+        try {
+            const invoiceId = await axios.post('https://staging.bitpay.com/checkout', {
+                action: 'checkout',
+                posData: '',
+                data: 'YzNHKdC1iQeZoR3gfFxF1N0JyhglsfO6hyFIawpMSUVdNRBBiu2A8/EdM6E0GqvQJ7VeSdeL5+KZTkEo+/eaiOR2mMH79hzVdCPfwHvcyECNibmB0WQTrqliB4abGlRYtplunFqpg2XnFzzcDSvHPQ=='
+            });
+            bitpay.showInvoice(invoiceId);
+        } catch (err) {
+            console.log(err);
+        }
     }
 })();

832e1efae61124ec6f7eb7ecdc4779adeb5e6369
Justin Kook <jkook@bitpay.com>
2020-09-10 19:45:47 -0400

    add css and point to POST /invoices

--- app.js
+++ app.js
@@ -1,67 +1,67 @@
-(function() {
-    const bitpayForm = document.getElementById('bitpayButton');
+const bitpayButton = document.getElementById('bitpayButton')
+bitpayButton.addEventListener('click', generateInvoice);
+
+async function generateInvoice(event) {
+    event.preventDefault();
+    const price = document.getElementById('invoicePrice').value;
+    const token = document.getElementById('apiToken').value;
     const redirectURL = 'https://justinkook.github.io/bitpayTestMerchant';
     const invoice = {
         currency: 'USD',
-        price: 120.2,
+        price,
+        token,
         itemDesc: 'Marlboro 36 Count Party Pack',
         orderId: '10742',
-        redirectURL,
-        // paymentCurrencies: [
-        //   'BTC',
-        //   'BCH',
-        //   'XRP',
-        //   'ETH',
-        //   'USDC',
-        //   'GUSD',
-        //   'PAX',
-        //   'BUSD',
-        // ],
-      };
-
-    bitpayForm.addEventListener('click', generateInvoice);
-    async function generateInvoice(event) {
-        event.preventDefault();
-        try {
-            const authOptions = {
-                method: 'POST',
-                url: 'https://testinvoice.b-pay.net/invoice',
-                headers: {
-                    'Authorization': 'Bearer RjZLS2pyUkd1elNONUh3N1NLUXRBRjJmVExXRFZzb3BaSkVWdUpnajZuZlI6',
-                    'Content-Type': 'application/json',
-                    'x-accept-version': '2.0.0'
-                },
-                body: JSON.stringify(invoice)
-              };
-            const { data } = await axios(authOptions);
-            const { id } = data;
-            // Modal
-            showInvoice(id);
-        } catch (err) {
-            console.log(err);
-        }
+        redirectURL
+    };
+    try {
+        const authOptions = {
+            method: 'POST',
+            url: 'https://staging.bitpay.com/invoices',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-accept-version': '2.0.0'
+            },
+            body: JSON.stringify(invoice)
+            };
+        const { data } = await axios(authOptions);
+        const { id } = data;
+        // Modal
+        showInvoice(id);
+    } catch (err) {
+        console.log(err);
     }
+}
 
-    function showInvoice(id) {
-        let is_paid = false
-        window.addEventListener("message", function (event) {
-            payment_status = event.data.status;
-            if (payment_status == "paid") {
-                is_paid = true
-                alert('successfully paid!');
-                //take action PAID
-            return;
-            } 
-        }, false);
-        //show the order info
-        bitpay.onModalWillLeave(function () {
-            if (is_paid == false) {
-                alert('Are you sure you want to leave?');
-                //take action, NOT PAID
-            } //endif
-        });
-        //show the modal
-        bitpay.enableTestMode();
-        bitpay.showInvoice(id);
-    }
-})();
+function showInvoice(id) {
+    let is_paid = false
+    window.addEventListener("message", function (event) {
+        payment_status = event.data.status;
+        if (payment_status == "paid") {
+            is_paid = true
+            const paidScreen = document.getElementById('paidScreen');
+            // Add the 'show' class to DIV
+            paidScreen.className = 'show';
+            // After 3 seconds, remove the show class from DIV
+            setTimeout(function () {
+                paidScreen.className = paidScreen.className.replace('show', ''); 
+            }, 3000);
+            //take action PAID
+        return;
+        } 
+    }, false);
+    //show the order info
+    bitpay.onModalWillLeave(function () {
+        if (is_paid == false) {
+            const snackbar = document.getElementById('snackbar');
+            snackbar.className = 'show';
+            setTimeout(function () {
+                snackbar.className = snackbar.className.replace('show', ''); 
+            }, 3000);
+            //take action, NOT PAID
+        } //endif
+    });
+    //show the modal
+    bitpay.enableTestMode();
+    bitpay.showInvoice(id);
+}

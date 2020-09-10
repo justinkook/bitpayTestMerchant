76ae63e8e5d5d9a30a3c1ef0e64bee07fa7c1356
Justin Kook <jkook@bitpay.com>
2020-09-10 14:21:56 -0400

    showInvoice in modal

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-(function() {
-    // https://test.bitpay.com/checkout
-    // bitpay.showInvoice(id);
-})()
+async function generateInvoice() {
+    const invoiceId = await axios.post('https://staging.bitpay.com/checkout', {
+        action: 'checkout',
+        postData: '',
+        data: 'YzNHKdC1iQeZoR3gfFxF1N0JyhglsfO6hyFIawpMSUVdNRBBiu2A8/EdM6E0GqvQJ7VeSdeL5+KZTkEo+/eaiOR2mMH79hzVdCPfwHvcyECNibmB0WQTrqliB4abGlRYtplunFqpg2XnFzzcDSvHPQ=='
+    });
+    bitpay.showInvoice(invoiceId);
+}
